e7aab7413a7e0fc6e0a21f3f75c9b32a7148eb26
Aiden Cullo <aidencullo@gmail.com>
2023-05-13 11:16:48 -0500

    deleting all components -- starting fresh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { MagnifierDirective } from './magnifier.directive';
-import { YearPipe } from './year.pipe';
-import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MagnifierDirective,
-    YearPipe,
-    ButtonComponent,
   ],
   imports: [
     BrowserModule,
