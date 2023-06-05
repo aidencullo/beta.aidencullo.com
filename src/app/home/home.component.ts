41a56cb0a7d5b77fd44ee149402d390524375352
Aiden Cullo <aidencullo@gmail.com>
2023-06-05 19:41:52 -0400

    styled homepage content

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
+    title = "aiden·cullo";
+    subTitle = "programmer & traveler";
+    description = "I’m a software engineer based out of NYC. I have professional webdev experience in React, Vue, and Node. I am interested in using technology to remedy social issues such as housing, immigration, and education.";
+    personals = [
+        {
+            name: "github",
+            link: "https://github.com/aidencullo",
+        },
+        {
+            name: "stack",
+            link: "https://stackoverflow.com/users/6447634/aiden-cullo",
+        },
+        {
+            name: "linkedin",
+            link: "https://www.linkedin.com/in/aiden-cullo-93ba02a4",
+        }
+    ];
 }
