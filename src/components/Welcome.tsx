e3b42970fd0a2c5b92055a16c6bd1602d0a8700a
aidencullo <aidencullo@gmail.com>
2025-08-13 14:24:52 -0400

    Add social and professional links below Welcome text - positioned out of sight

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Welcome: React.FC = () => {
   return (
-    <h1 className="name">Welcome</h1>
+    <div className="welcome-container">
+      <h1 className="name">Welcome</h1>
+      <div className="links-container">
+        <a href="https://github.com/aidencullo" target="_blank" rel="noopener noreferrer" className="link">
+          GitHub
+        </a>
+        <a href="https://stackoverflow.com/users/6447634/aiden-cullo" target="_blank" rel="noopener noreferrer" className="link">
+          Stack Overflow
+        </a>
+        <a href="https://www.linkedin.com/in/aidencullo" target="_blank" rel="noopener noreferrer" className="link">
+          LinkedIn
+        </a>
+        <a href="mailto:aidencullo@gmail.com" className="link">
+          Email
+        </a>
+      </div>
+    </div>
   )
 }
 
