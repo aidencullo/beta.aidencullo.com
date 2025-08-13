adc086b800f421e8e70fd32b95a43f32684f977b
aidencullo <aidencullo@gmail.com>
2025-08-13 14:57:38 -0400

    Remove name from header for cleaner design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,6 @@ const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="header-content">
-        <a href="/" className="header-name">
-          Aiden Cullo
-        </a>
       </div>
     </header>
   )
