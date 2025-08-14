71a21d53521394484b403cdd0e885bde343efc82
aidencullo <aidencullo@gmail.com>
2025-08-14 17:24:17 -0400

    Revert favicon changes, keep original SVG icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="header-content">
-        <a href="/" className="header-favicon">
+        <a href="#" className="header-favicon">
           <img src="/favicon.svg" alt="Favicon" />
         </a>
         <a href="#bio" className="header-me-link">me</a>
