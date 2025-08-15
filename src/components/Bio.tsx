849e7c057c341397887699825795109051d99355
aidencullo <aidencullo@gmail.com>
2025-08-15 11:47:05 -0400

    Add 'Hello!' to bio text and update page title to 'Loading...'

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -13,7 +13,7 @@ const Bio: React.FC = () => {
       </div>
       
       <div className="bio-text">
-        <p>My name is Aiden. I'm a software engineer based out of Brooklyn. I am a full stack engineer at JP Morgan where I'm developing a firm-wide LLM tracking system.</p>
+        <p>Hello! My name is Aiden. I'm a software engineer based out of Brooklyn. I am a full stack engineer at JP Morgan where I'm developing a firm-wide LLM tracking system.</p>
       </div>
       
       <div className="bio-links">
