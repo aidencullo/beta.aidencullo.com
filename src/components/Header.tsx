301966650bef2f53fe34119039b64743a6a4e9a6
aidencullo <aidencullo@gmail.com>
2025-08-19 11:44:40 -0400

    Remove logo from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,6 @@ const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="header-content">
-        <InternalLink href="#" title="Home" className="header-favicon">
-          <img src="/favicon.svg" alt="Favicon" />
-        </InternalLink>
         <InternalLink href="#bio" title="About me" className="header-me-link">me</InternalLink>
       </div>
     </header>
