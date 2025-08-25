a2f5a3a9f0d5aeeb16eaf89a6a7665dee1eb5bc0
aidencullo <aidencullo@gmail.com>
2025-08-25 15:57:11 -0400

    Update ThemeButton component

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ThemeButton: React.FC = () => {
+
+  const [theme, setTheme] = useState("light")
+
   const changeTheme = () => {
     if (document.documentElement.getAttribute('data-theme') === 'light') {
+      setTheme("dark")
       document.documentElement.setAttribute('data-theme', 'dark');
     } else {
+      setTheme("light")
       document.documentElement.setAttribute('data-theme', 'light');
     }
   }
 
   return (
     <button onClick={changeTheme} className="header-me-link" style={{ background: 'none', border: 'none', cursor: 'pointer' }}>
-      change theme
+      {theme === "light" ? '🌙' : '🌞'}
     </button>
   )
 }
