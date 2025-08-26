c4a2b1f74034defd7e67fb85269c431eb905326f
aidencullo <aidencullo@gmail.com>
2025-08-26 13:26:33 -0400

    Update useTheme hook

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -2,11 +2,9 @@ import { useEffect, useState } from "react";
 
 function useTheme() {
   const [theme, setTheme] = useState<string>()
-  console.log('theme after state', theme)
 
   useEffect(() => {
     const theme = localStorage.getItem('theme')
-    console.log('initial useEffect theme', theme)
     if (theme) {
       setTheme(theme)
     }
@@ -16,7 +14,6 @@ function useTheme() {
     if (theme) {
       document.documentElement.setAttribute('data-theme', theme)
       localStorage.setItem('theme', theme)
-      console.log('dependency useEffect theme', theme)
     }
   }, [theme])
 
