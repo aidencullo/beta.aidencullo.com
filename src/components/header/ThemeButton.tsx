7fdf2dd263ce44f967d1606cded51077ae948bb2
aidencullo <aidencullo@gmail.com>
2025-08-28 12:15:53 -0400

    Move ThemeButton component to header folder and update import references

--- src/components/header/ThemeButton.tsx
+++ src/components/header/ThemeButton.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useTheme } from '../hooks/useTheme'
+import { useTheme } from '../../hooks/useTheme'
 import { FaSun, FaMoon } from 'react-icons/fa'
 
 const ThemeButton: React.FC = () => {
