cfceb875376251471b44f47cee9adaa431324809
aidencullo <aidencullo@gmail.com>
2025-08-29 14:52:17 -0400

    Add variables for image path and alt text in ProfilePicture component

--- src/components/Body/Bio/ProfilePicture/ProfilePicture.tsx
+++ src/components/Body/Bio/ProfilePicture/ProfilePicture.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import './ProfilePicture.css'
 
 const ProfilePicture: React.FC = () => {
+  const imagePath = "/profile.jpg"
+  const altText = "Aiden Cullo"
+  
   return (
     <div className="profile-picture">
-      <img src="/profile.jpg" alt="Aiden Cullo" />
+      <img src={imagePath} alt={altText} />
     </div>
   )
 }
