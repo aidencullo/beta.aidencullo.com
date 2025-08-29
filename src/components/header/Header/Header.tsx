cca6df7410043611d057261b3f4bd71a5908c5fe
aidencullo <aidencullo@gmail.com>
2025-08-29 09:18:00 -0400

    Refactor Header component to use HeaderLeft and HeaderRight components

--- src/components/header/Header/Header.tsx
+++ src/components/header/Header/Header.tsx
@@ -1,19 +1,14 @@
 import React from 'react'
-import InternalLink from '@links/InternalLink/InternalLink'
-import ThemeButton from './ThemeButton/ThemeButton'
+import HeaderLeft from './HeaderLeft/HeaderLeft'
+import HeaderRight from './HeaderRight/HeaderRight'
 import './Header.css'
-import LanguageSelector from './LanguageSelector/LanguageSelector'
 
 const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="header-content">
-        <div className="header-navigation">
-          <InternalLink href="#" title="Home" className="header-home-link">home</InternalLink>
-          <InternalLink href="#bio" title="About" className="header-about-link">about</InternalLink>
-        </div>
-        <ThemeButton />
-        <LanguageSelector />
+        <HeaderLeft />
+        <HeaderRight />
       </div>
     </header>
   )
