d011cd1e9ca90b1af6e251b2c96fe3b04a0b953d
aidencullo <aidencullo@gmail.com>
2025-08-29 09:08:50 -0400

    Update header component styling and structure

--- src/components/header/Header/HeaderLeft/HeaderLeft.tsx
+++ src/components/header/Header/HeaderLeft/HeaderLeft.tsx
@@ -5,10 +5,8 @@ import './HeaderLeft.css'
 const HeaderLeft: React.FC = () => {
   return (
     <div className="header-left">
-      <div className="header-navigation">
         <InternalLink href="#" title="Home" className="header-home-link">home</InternalLink>
         <InternalLink href="#bio" title="About" className="header-about-link">about</InternalLink>
-      </div>
     </div>
   )
 }
