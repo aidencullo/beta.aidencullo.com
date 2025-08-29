a2f6f9070d0eebf3231d8e5d7050846130c9ac5d
aidencullo <aidencullo@gmail.com>
2025-08-28 22:09:39 -0400

    Update LanguageSelector to use variables for language codes and unique CSS class

--- src/components/header/LanguageSelector/LanguageSelector.tsx
+++ src/components/header/LanguageSelector/LanguageSelector.tsx
@@ -4,14 +4,16 @@ import { useLanguageCustom } from '../../../hooks/useLanguage'
 
 const LanguageSelector: React.FC = () => {
   const { language, setLanguageCustom } = useLanguageCustom()
+  const englishCode = "en"
+  const spanishCode = "es"
 
   const changeLanguage = () => {
-    setLanguageCustom(language === "en" ? "es" : "en")
+    setLanguageCustom(language === englishCode ? spanishCode : englishCode)
   }
 
   return (
-    <div onClick={changeLanguage} className="header-me-link theme-button" style={{ background: 'none', border: 'none', cursor: 'pointer' }}>
-      {language === "en" ? "es" : "en"}
+    <div onClick={changeLanguage} >
+      {language === englishCode ? spanishCode : englishCode}
     </div>
   )
 }
