77dff592f307368714c6ef80923889d9a9534e34
Aiden Cullo <aidencullo@gmail.com>
2025-11-16 20:48:45 -0500

    Remove GitLab, StackOverflow, and Blog links and components entirely

--- src/components/Body/Bio/BioLinks/BioLinks.tsx
+++ src/components/Body/Bio/BioLinks/BioLinks.tsx
@@ -1,23 +1,17 @@
 import React from 'react'
 import GitHubIconLink from '@links/social/github/GitHubIconLink/GitHubIconLink'
-import GitLabIconLink from '@links/social/gitlab/GitLabIconLink/GitLabIconLink'
-import StackOverflowIconLink from '@links/social/stackoverflow/StackOverflowIconLink/StackOverflowIconLink'
 import LinkedInIconLink from '@links/social/linkedin/LinkedInIconLink/LinkedInIconLink'
 import EmailIconLink from '@links/social/email/EmailIconLink/EmailIconLink'
 import ResumeIconLink from '@links/social/resume/ResumeIconLink/ResumeIconLink'
-import BlogIconLink from '@links/social/blog/BlogIconLink/BlogIconLink'
 import './BioLinks.css'
 
 const BioLinks: React.FC = () => {
   return (
     <div className="bio-links">
       <GitHubIconLink />
-      {/* <GitLabIconLink />
-      <StackOverflowIconLink /> */}
       <LinkedInIconLink />
       <EmailIconLink />
       <ResumeIconLink />
-      {/* <BlogIconLink /> */}
     </div>
   )
 }
--- src/components/Footer/FooterBottom/FooterBottomLinks/FooterBottomLinks.tsx
+++ src/components/Footer/FooterBottom/FooterBottomLinks/FooterBottomLinks.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import GitHubTextLink from '@links/social/github/GitHubTextLink/GitHubTextLink';
-import StackOverflowTextLink from '@links/social/stackoverflow/StackOverflowTextLink/StackOverflowTextLink';
 import LinkedInTextLink from '@links/social/linkedin/LinkedInTextLink/LinkedInTextLink';
 import EmailTextLink from '@links/social/email/EmailTextLink/EmailTextLink';
 import ResumeTextLink from '@links/social/resume/ResumeTextLink/ResumeTextLink';
-import BlogTextLink from '@links/social/blog/BlogTextLink/BlogTextLink';
 import FooterBottomLink from '../FooterBottomLink/FooterBottomLink';
 
 const FooterBottomLinks: React.FC = () => {
   const footerBottomLinks = [
     GitHubTextLink,
-    StackOverflowTextLink,
     LinkedInTextLink,
     EmailTextLink,
-    ResumeTextLink,
-    BlogTextLink
+    ResumeTextLink
   ];
 
   return (
